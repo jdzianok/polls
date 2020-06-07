36584b717da2f8828b2b37726290723ebfccad7f
Jakub <j.dzianok@gmail.com>
2020-06-07 16:10:43 +0200

    add Tabs component

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,32 @@
 <script>
   import Header from './components/Header.svelte';
   import Footer from './components/Footer.svelte';
+  import Tabs from './blocks/Tabs.svelte';
+
+  let items = ['Current Polls', 'Add New Poll'];
+  let activeItem = 'Current Polls';
+
+  const tabChange = e => {
+    activeItem = e.detail;
+
+  }
 </script>
 
 <Header />
 <main>
-
+  <Tabs {activeItem} {items} on:tabChange={tabChange}/>
+  {#if activeItem === "Current Polls"}
+     <p>Poll list component goes here</p>
+  {:else if activeItem === "Add New Poll"}
+    <p>New poll form component goes here</p>
+  {/if}
 </main>
 <Footer />
-
-
-
 
 <style>
   main {
     max-width: 960px;
     margin: 40px auto;
+    
   }
 </style>

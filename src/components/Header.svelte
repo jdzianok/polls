6605c4975ee0a96866c116bacecd4e40ec12d67b
Jakub <j.dzianok@gmail.com>
2020-06-07 14:50:38 +0200

    initial commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,21 @@
+<header>
+  <h1>
+    <img src="/assets/poll_logo.svg" alt="poll logo">
+  </h1>
+</header>
+
+<style>
+  header {
+    background-color: #f7f7f7;
+    padding: 20px;
+  }
+
+  h1 {
+    margin: 0;
+    text-align: center;
+  }
+
+  img {
+    max-width: 150px;
+  }
+</style>
